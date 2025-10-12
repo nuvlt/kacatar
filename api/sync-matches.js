29ffed9cd61a41cb5b4d1b17cb905cbf7949908a
nuvlt <152844233+nuvlt@users.noreply.github.com>
2025-10-12 15:38:47 +0300

    Update sync-matches.js

--- api/sync-matches.js
+++ api/sync-matches.js
@@ -1,4 +1,5 @@
-import fetch from "node-fetch";
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
